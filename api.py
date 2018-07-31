b8027522a2df385ed6561db05b0cea96cad8ded2
olamijinadebayo <consultolamijin@gmail.com>
2018-07-31 13:44:31 +0100

    This will add a function to create a list of all job builds and also authenticate the user credentials before the script is being run

--- api.py
+++ api.py
@@ -40,3 +40,56 @@ class Jobs(Base):
     result = Column(String)
     building = Column(String)
     estimatedDuration = Column(String)
+
+def createJobList(start, lastBuildNumber, jobName):
+    jList = []
+    for i in range(start + 1, lastBuildNumber + 1):
+        current = server.get_build_info(jobName, i)
+        current_as_jobs = Jobs()
+        current_as_jobs.jen_id = current['id']
+        current_as_jobs.building = current['building']
+        current_as_jobs.estimatedDuration = current['estimatedDuration']
+        current_as_jobs.name = jobName
+        current_as_jobs.result = current['result']
+        current_as_jobs.timeStamp = datetime.datetime.fromtimestamp(int(current['timestamp'])*0.001)
+        jList.append(current_as_jobs)
+    return jList
+
+
+url = 'http://localhost:8080'
+username ="ola"
+password ="1234abcdola"
+server = connectToJenkins(url, username, password)
+
+authenticated = false
+try:
+    server.get_whoami()
+    authenticated = true
+except jenkins.JenkinsException as e:
+    print ('Authentication error')
+    authenticated = false
+
+if authenticated:
+    session = initializeDb()
+
+    # get a list of all jobs
+    jobs = server.get_all_jobs()
+
+    for j in jobs:
+        jobName = j['name'] # get job name
+        print (jobName)
+        lastJobId = getLastJobId(session, jobName) # get last locally stored job of this name
+
+        lastBuildNumber = server.get_job_info(jobName)['lastBuild']['number']  # get last build number from Jenkins for this job
+
+        # if job not stored, update the db with all entries
+        if lastJobId == None:
+            start = 0
+        # if job exists, update the db with new entrie
+        else:
+            start = lastJobId
+
+        # create a list of unadded job objects
+        jlist = createJobList(start, lastBuildNumber, jobName)
+        # add job to db
+        addJob(session, jlist)
